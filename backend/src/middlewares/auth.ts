fb9f6b76ac89cd88cfd45357ffb28af75ae07723
ryuga <rahul0singh001@gmail.com>
2024-03-05 14:11:25 +0530

    initial commit

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -6,12 +6,12 @@ import { TryCatch } from "./error.js";
 export const adminOnly = TryCatch(async (req, res, next) => {
   const { id } = req.query;
 
-  if (!id) return next(new ErrorHandler("Saale Login Kr phle", 401));
+  if (!id) return next(new ErrorHandler("Not Signed In ", 401));
 
   const user = await User.findById(id);
-  if (!user) return next(new ErrorHandler("Saale Fake ID Deta Hai", 401));
+  if (!user) return next(new ErrorHandler("Invalid Id or Password", 401));
   if (user.role !== "admin")
-    return next(new ErrorHandler("Saale Aukat Nhi Hai Teri", 403));
+    return next(new ErrorHandler("Access Restricted", 403));
 
   next();
 });
